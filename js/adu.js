4e0e1afeeb9abd6d140efa0f640a24481f23c477
savmuv <63292726+savmuv@users.noreply.github.com>
2021-12-28 22:38:45 +0700

    Delete adu.js

--- js/adu.js
+++
@@ -1,8 +0,0 @@
-	atOptions = {
-		'key' : 'e50656314f5a73de24a452da7431f110',
-		'format' : 'iframe',
-		'height' : 600,
-		'width' : 160,
-		'params' : {}
-	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://sinisterbatchoddly.com/e50656314f5a73de24a452da7431f110/invoke.js"></scr' + 'ipt>');
