5f52d9af87dcd8b88a81fe99b54e61383fbe84ef
savmuv <63292726+savmuv@users.noreply.github.com>
2021-12-28 21:21:41 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ function init() {
    <div id="nav">
    </div>
 </header>
-<div style="left:10px;position:fixed;width:160px;height:100%;"></div>
+<div style="left:10px;position:fixed;width:160px;height:100%;"><script type="text/javascript">atOptions = {'key' : 'e50656314f5a73de24a452da7431f110','format' : 'iframe','height' : 600,'width' : 160,'params' : {}};document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://sinisterbatchoddly.com/e50656314f5a73de24a452da7431f110/invoke.js"></scr' + 'ipt>');</script></div>
 <div>
 <div id="content" style="padding-top: ${UI.header_padding}px;${UI.fixed_footer ?' padding-bottom: clamp(170px, 100%, 300px);': ''}">
 </div>
