7929b74e2170adf52e0721079470466e0abc608d
savmuv <63292726+savmuv@users.noreply.github.com>
2021-12-29 23:42:41 +0700

    Update app-advanced.js

--- js/app-advanced.js
+++ js/app-advanced.js
@@ -3,11 +3,8 @@
   // Initialize the page
   function init() {
       document.siteName = $('title').html();
-      var html = `<div style="top:100px;left:100px;position:fixed;max-width:160px;height:100%">
-  <script type="text/javascript">
-	atOptions = {'key' : 'e50656314f5a73de24a452da7431f110','format' : 'iframe','height' : 600,'width' : 160,'params' : {}};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://sinisterbatchoddly.com/e50656314f5a73de24a452da7431f110/invoke.js"></scr' + 'ipt>');
-  </script>
+      var html = `<div style="top:100px;left:100px;position:fixed">
+  <iframe src="//sinisterbatchoddly.com/watchnew?key=e50656314f5a73de24a452da7431f110" width="160" height="600" frameborder="0" scrolling="no"></iframe>
   </div>
       <header>
      <div id="nav">
