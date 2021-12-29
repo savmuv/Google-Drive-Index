f4641a2d80eccdbbce584aa21bca8dba724a1c04
savmuv <63292726+savmuv@users.noreply.github.com>
2021-12-29 22:15:36 +0700

    Update app-advanced.js

--- js/app-advanced.js
+++ js/app-advanced.js
@@ -9,7 +9,8 @@
   </header>
   <div>
   <div style="top:100px;left:100px;position:fixed">
-  <script type="text/javascript" src="//sinisterbatchoddly.com/e50656314f5a73de24a452da7431f110/invoke.js">atOptions = {'key' : 'e50656314f5a73de24a452da7431f110','format' : 'iframe','height' : 600,'width' : 160,'params' : {}};</script>
+  <script type="text/javascript">atOptions = {'key' : 'e50656314f5a73de24a452da7431f110','format' : 'iframe','height' : 600,'width' : 160,'params' : {}};</script>
+  <script type="text/javascript" src="//sinisterbatchoddly.com/e50656314f5a73de24a452da7431f110/invoke.js"></script>
   </div>
   </div>
   <div id="content" style="padding-top: ${UI.header_padding}px;${UI.fixed_footer ?' padding-bottom: clamp(170px, 100%, 300px);': ''}">
