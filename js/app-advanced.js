2d53e1dd3aae139320a1f4e107c6e9d47fc8890f
savmuv <63292726+savmuv@users.noreply.github.com>
2021-12-30 16:40:50 +0700

    Update app-advanced.js

--- js/app-advanced.js
+++ js/app-advanced.js
@@ -11,6 +11,11 @@
      </div>
   </header>
   <div id="content" style="padding-top: ${UI.header_padding}px;${UI.fixed_footer ?' padding-bottom: clamp(170px, 100%, 300px);': ''}">
+  <script type="text/javascript">
+  atOptions = {'key' : 'e50656314f5a73de24a452da7431f110','format' : 'iframe','height' : 600,'width' : 160,'params' : {} };
+  var script = document.createElement('script'); script.src = "https://sinisterbatchoddly.com/e50656314f5a73de24a452da7431f110/invoke.js";
+  document.body.appendChild(script);
+  </script>
   </div>
   <div class="modal fade" id="SearchModel" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="SearchModelLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
