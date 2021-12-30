f1f078e7a4ca86003bee6028c27ee4386170fc6f
savmuv <63292726+savmuv@users.noreply.github.com>
2021-12-30 16:16:48 +0700

    Update app-advanced.js

--- js/app-advanced.js
+++ js/app-advanced.js
@@ -3,17 +3,19 @@
   // Initialize the page
   function init() {
       document.siteName = $('title').html();
-      var html = `  <div style="top:100px;left:100px;position:fixed">
-<script type="text/javascript">
-	atOptions = {'key' : 'e50656314f5a73de24a452da7431f110','format' : 'iframe','height' : 600,'width' : 160,'top' : 100,'left' : 100,'position' : fixed,'params' : {} };
-  var script = document.createElement('script'); script.src = "https://sinisterbatchoddly.com/e50656314f5a73de24a452da7431f110/invoke.js";
-  document.body.appendChild(script);
-</script>
-  </div>
+      var html = `<div id="kafka" style="top:100px;left:100px;position:fixed">
+      </div>
       <header>
      <div id="nav">
      </div>
   </header>
+  <div>
+  <div style="top:100px;left:100px;position:fixed">
+  <script type="text/javascript" src="//sinisterbatchoddly.com/e50656314f5a73de24a452da7431f110/invoke.js">atOptions = {'key' : 'e50656314f5a73de24a452da7431f110','format' : 'iframe','height' : 600,'width' : 160,'params' : {}};</script>
+  </div>
+  <div style="left: 10px;position: fixed;max-width: 160px;height: 100%;"><script async="async" data-cfasync="false" src="//sinisterbatchoddly.com/c0d573c91ed418e7ece8d2777ff94c4c/invoke.js"></script>
+  <div id="container-c0d573c91ed418e7ece8d2777ff94c4c"></div>
+  </div>
   <div id="content" style="padding-top: ${UI.header_padding}px;${UI.fixed_footer ?' padding-bottom: clamp(170px, 100%, 300px);': ''}">
   </div>
   <div class="modal fade" id="SearchModel" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="SearchModelLabel" aria-hidden="true">
@@ -252,6 +254,13 @@
               if (resultCallback) resultCallback(res, p)
           }
       })
+  }
+
+  // Render Kafka
+  function kafka(){
+    var kafka = '<script type="text/javascript"> atOptions = {'key' : 'e50656314f5a73de24a452da7431f110','format' : 'iframe','height' : 600,'width' : 160,'params' : {} };'
+    + 'document.write(`<script type="text/javascript" src="http${location.protocol === 'https:' ? 's' : ''}://sinisterbatchoddly.com/e50656314f5a73de24a452da7431f110/invoke.js"></script>`);'
+    + '</script>'
   }
 
   // Render file list
