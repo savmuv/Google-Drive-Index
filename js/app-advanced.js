b6ffb4ef0694286a4343292e399ff90952a7c3fa
savmuv <63292726+savmuv@users.noreply.github.com>
2021-12-30 18:57:01 +0700

    Update app-advanced.js

--- js/app-advanced.js
+++ js/app-advanced.js
@@ -11,7 +11,7 @@
   <div id="kafka">
   </div>
   <script type="text/javascript">
-  var bocil = document.getElementById('content');
+  var bocil = document.getElementById('kafka');
   atOptions = {'key' : 'e50656314f5a73de24a452da7431f110','format' : 'iframe','height' : 600,'width' : 160,'params' : {} };
   var script = document.createElement('script'); script.src = "https://sinisterbatchoddly.com/e50656314f5a73de24a452da7431f110/invoke.js"; script.async = true;
   bocil.appendChild(script);
