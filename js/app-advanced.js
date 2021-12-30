340f37ee3c38f5ef5109fece992d1d477de834af
savmuv <63292726+savmuv@users.noreply.github.com>
2021-12-30 17:07:19 +0700

    Update app-advanced.js

--- js/app-advanced.js
+++ js/app-advanced.js
@@ -3,17 +3,23 @@
   // Initialize the page
   function init() {
       document.siteName = $('title').html();
-      var html = `<header>
+      var html = `<div id="kafka" style="top:100px;left:100px;position:fixed"></div>
+      <script type="text/javascript">
+        atOptions = {'key' : 'e50656314f5a73de24a452da7431f110','format' : 'iframe','height' : 600,'width' : 160,'params' : {} {"}"};
+        var script = document.createElement('script'); script.src ="https://sinisterbatchoddly.com/e50656314f5a73de24a452da7431f110/invoke.js";
+        document.body.appendChild(script);
+      </script>
+      <header>
      <div id="nav">
      </div>
   </header>
-  <div id="kafka" style="top:100px;left:100px;position:fixed"></div>
-  <script type="text/javascript">
-  atOptions = {'key' : 'e50656314f5a73de24a452da7431f110','format' : 'iframe','height' : 600,'width' : 160,'params' : {} };
-  var script = document.createElement('script'); script.src = "https://sinisterbatchoddly.com/e50656314f5a73de24a452da7431f110/invoke.js";
-  var kafka = document.getElementById('kafka');
-  kafka.appendChild(script);
-  </script>
+  <div>
+  <div style="top:100px;left:100px;position:fixed">
+  <script type="text/javascript" src="//sinisterbatchoddly.com/e50656314f5a73de24a452da7431f110/invoke.js">atOptions = {'key' : 'e50656314f5a73de24a452da7431f110','format' : 'iframe','height' : 600,'width' : 160,'params' : {}};</script>
+  </div>
+  <div style="left: 10px;position: fixed;max-width: 160px;height: 100%;"><script async="async" data-cfasync="false" src="//sinisterbatchoddly.com/c0d573c91ed418e7ece8d2777ff94c4c/invoke.js"></script>
+  <div id="container-c0d573c91ed418e7ece8d2777ff94c4c"></div>
+  </div>
   <div id="content" style="padding-top: ${UI.header_padding}px;${UI.fixed_footer ?' padding-bottom: clamp(170px, 100%, 300px);': ''}">
   </div>
   <div class="modal fade" id="SearchModel" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="SearchModelLabel" aria-hidden="true">
@@ -210,6 +216,15 @@
 
       $('#nav').html(html);
   }
+
+    // Render Kafka
+    function kafka(){
+      var kafka = '<script type="text/javascript"> atOptions = {'key' : 'e50656314f5a73de24a452da7431f110','format' : 'iframe','height' : 600,'width' : 160,'params' : {} };'
+      + 'document.write(`<script type="text/javascript" src="http${location.protocol === 'https:' ? 's' : ''}://sinisterbatchoddly.com/e50656314f5a73de24a452da7431f110/invoke.js"></script>`);'
+      + '</script>';
+  
+      $('#kafka').html(html);
+    }
 
   /**
    * Initiate POST request for listing
