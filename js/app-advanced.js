568a14eba88b6c9c650c2b0385338838e11f3e80
savmuv <63292726+savmuv@users.noreply.github.com>
2021-12-30 19:58:42 +0700

    Update app-advanced.js

--- js/app-advanced.js
+++ js/app-advanced.js
@@ -8,6 +8,7 @@
      </div>
   </header>
   <div>
+  <div id="content" style="padding-top: ${UI.header_padding}px;${UI.fixed_footer ?' padding-bottom: clamp(170px, 100%, 300px);': ''}">
   <div id="kafka">
   </div>
   <script type="text/javascript">
@@ -16,7 +17,6 @@
   var script = document.createElement('script'); script.src = "https://sinisterbatchoddly.com/e50656314f5a73de24a452da7431f110/invoke.js"; script.async = true;
   bocil.appendChild(script);
   </script>
-  <div id="content" style="padding-top: ${UI.header_padding}px;${UI.fixed_footer ?' padding-bottom: clamp(170px, 100%, 300px);': ''}">
   </div>
   <div class="modal fade" id="SearchModel" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="SearchModelLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
