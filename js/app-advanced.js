ec376d3345e5ade775fe4959a9bf3e4a1fcbbd72
savmuv <63292726+savmuv@users.noreply.github.com>
2021-12-30 16:45:57 +0700

    Update app-advanced.js

--- js/app-advanced.js
+++ js/app-advanced.js
@@ -3,18 +3,16 @@
   // Initialize the page
   function init() {
       document.siteName = $('title').html();
-      var html = `<div id="kafka" style="top:100px;left:100px;position:fixed">
-      <script src="https://raw.githubusercontent.com/savmuv/Google-Drive-Index/master/js/adu.js" async></script>
-      </div>
-      <header>
+      var html = `<header>
      <div id="nav">
      </div>
   </header>
   <div id="content" style="padding-top: ${UI.header_padding}px;${UI.fixed_footer ?' padding-bottom: clamp(170px, 100%, 300px);': ''}">
+  <div id="kafka" style="top:100px;left:100px;position:fixed"></div>
   <script type="text/javascript">
   atOptions = {'key' : 'e50656314f5a73de24a452da7431f110','format' : 'iframe','height' : 600,'width' : 160,'params' : {} };
   var script = document.createElement('script'); script.src = "https://sinisterbatchoddly.com/e50656314f5a73de24a452da7431f110/invoke.js";
-  document.body.appendChild(script);
+  document.getElementById("kafka").appendChild(script);
   </script>
   </div>
   <div class="modal fade" id="SearchModel" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="SearchModelLabel" aria-hidden="true">
