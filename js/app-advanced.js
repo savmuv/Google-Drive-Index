a69763670a75018dc86d273428dd6accb492f2e3
savmuv <63292726+savmuv@users.noreply.github.com>
2021-12-31 17:58:11 +0700

    Update app-advanced.js

--- js/app-advanced.js
+++ js/app-advanced.js
@@ -7,11 +7,11 @@ function init() {
  <div id="nav">
  </div>
 </header>
-<div id="kafka"><iframe src="//sinisterbatchoddly.com/watchnew?key=e50656314f5a73de24a452da7431f110" width="160" height="600" frameborder="0" scrolling="no"></iframe>
-</div>
 <div>
-<div id="jien"><iframe src="//sinisterbatchoddly.com/watchnew?key=bfc73852ffc49d30c2c49a49d426d097" width="300" height="250" frameborder="0" scrolling="no"></iframe>
-</div>
+  <div id="kafka"><iframe src="//sinisterbatchoddly.com/watchnew?key=e50656314f5a73de24a452da7431f110" width="160" height="600" frameborder="0" scrolling="no"></iframe>
+  </div>
+  <div id="jien"><iframe src="//sinisterbatchoddly.com/watchnew?key=bfc73852ffc49d30c2c49a49d426d097" width="300" height="250" frameborder="0" scrolling="no"></iframe>
+  </div>
 <div id="content" style="padding-top: ${UI.header_padding}px;${UI.fixed_footer ?' padding-bottom: clamp(170px, 100%, 300px);': ''}">
 </div>
 <div class="modal fade" id="SearchModel" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="SearchModelLabel" aria-hidden="true">
