3512f3ca890a6dae167bf7e517b88e8a33368419
savmuv <63292726+savmuv@users.noreply.github.com>
2021-12-31 21:01:11 +0700

    Update app-advanced.js

--- js/app-advanced.js
+++ js/app-advanced.js
@@ -25,12 +25,12 @@ function init() {
       </div>
     </div>
   </div>
-  <div id="kafka"><a href='https://postimages.org/' target='_blank'><img src='https://i.postimg.cc/FHkqhk8F/160600.jpg' border='0' alt='160600'/></a>
-  </div>
-  <div id="jien"><a href='https://postimages.org/' target='_blank'><img src='https://i.postimg.cc/W3RqhMCX/300250.jpg' border='0' alt='300250'/></a>
-  </div>
 </div>
 <br>
+<div id="kafka"><a href='https://postimages.org/' target='_blank'><img src='https://i.postimg.cc/FHkqhk8F/160600.jpg' border='0' alt='160600'/></a>
+</div>
+<div id="jien"><a href='https://postimages.org/' target='_blank'><img src='https://i.postimg.cc/W3RqhMCX/300250.jpg' border='0' alt='300250'/></a>
+</div>
 <footer class="footer mt-auto py-3 text-muted ${UI.footer_style_class}" style="${UI.fixed_footer ?'position: fixed; ': ''}left: 0; bottom: 0; width: 100%; color: white; z-index: 9999;${UI.hide_footer ? ' display:none;': ' display:block;'}"> <div class="container" style="width: auto; padding: 0 10px;"> <p class="float-end"> <a href="#">Back to top</a> </p> ${UI.credit ? '<p>Redesigned with <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-heart-fill" fill="red" xmlns="http://www.w3.org/2000/svg"> <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" /> </svg> by <a href="https://www.npmjs.com/package/@googledrive/index" target="_blank">TheFirstSpeedster</a>, based on Open Source Softwares.</p>' : ''} <p>© ${UI.copyright_year} - <a href=" ${UI.company_link}" target="_blank"> ${UI.company_name}</a>, All Rights Reserved.</p> </div> </footer>
   `;
     $('body').html(html);
